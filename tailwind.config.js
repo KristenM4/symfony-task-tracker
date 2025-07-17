f6f2bd4b966064deda2b14d920a7e304bd4c3d11
Kristen <kristen4810@gmail.com>
2025-07-17 22:45:01 +0100

    add tailwind and symphony/asset

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/styles/*.{js,jsx,ts,tsx,vue}",
+    "./templates/**/*.html.twig"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
